a9fa14158e05c4b9359956d90d877ad61a758649
guilherme ramalho <guilherme.ramalho@loungedv.net>
2019-06-26 17:17:08 -0300

    alteracao

--- ApplicationTesteRepository/Startup.cs
+++ ApplicationTesteRepository/Startup.cs
@@ -27,6 +27,7 @@ namespace ApplicationTesteRepository
 
             services.Configure<CookiePolicyOptions>(options =>
             {
+
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
